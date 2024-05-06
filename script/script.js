722f9892d4228ca3fb91a43468f8c94e2641ed15
rssotho <166599004+rssotho@users.noreply.github.com>
2024-05-06 14:55:07 +0200

    Button upload

---
+++ script/script.js
@@ -0,0 +1,5 @@
+document.getElementById('fileInput').addEventListener('change', function(e) {
+    var fileName = e.target.files[0].name;
+    var nextSibling = e.target.nextElementSibling;
+    nextSibling.innerText = fileName;
+  });
