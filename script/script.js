f34eb21ae3aacb1b87a142aba1763dec520ea69c
rssotho <166599004+rssotho@users.noreply.github.com>
2024-05-07 14:00:17 +0200

    Hide the search button until the it is triggered

--- script/script.js
+++ script/script.js
@@ -1,73 +1,76 @@
-const upload = document.querySelector('#load_data');
-const searchBtn = document.querySelector('#search');
-
-upload.addEventListener('click', function() 
+$(document).ready(function () 
 {
-    const fileInput = document.getElementById('fileInput');
-    const file = fileInput.files[0];
-    const reader = new FileReader();
+    const upload = document.querySelector('#load_data');
+    const searchBtn = document.querySelector('#search');
 
-    reader.onload = function(e) 
+    $('#search').hide();
+    $('#searchInput').hide();
+
+    upload.addEventListener('click', function () 
     {
-        const data = e.target.result;
-        const workbook = XLSX.read(data, { type: 'array' });
-        const sheetName = workbook.SheetNames[0];
-        const csvData = XLSX.utils.sheet_to_csv(workbook.Sheets[sheetName]);
-        displayData(csvData);
-    };
+        const fileInput = document.getElementById('fileInput');
+        const file = fileInput.files[0];
+        const reader = new FileReader();
 
-    reader.readAsArrayBuffer(file);
-});
-
-searchBtn.addEventListener('click', function ()
-{
-    const searchInput = document.getElementById('searchInput').value.toLowerCase();
-    const tableRows = document.querySelectorAll('#tableContainer tbody tr');
-
-    tableRows.forEach(row => 
-    {
-        const rowData = row.textContent.toLowerCase();
-        if (rowData.includes(searchInput)) 
+        reader.onload = function (e) 
         {
-            row.style.display = '';
-        } 
-        else 
-        {
-            row.style.display = 'none';
-        }
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: 'array' });
+            const sheetName = workbook.SheetNames[0];
+            const csvData = XLSX.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+            displayData(csvData);
+        };
+
+        
+        $('#search').show();
+        $('#searchInput').show();
+        reader.readAsArrayBuffer(file);
     });
-});
 
-// Displaying the content from the document.
-
-function displayData(data) 
-{
-    const parsedData = Papa.parse(data, { header: true }).data;
-    let tableHtml = '<table class="table"><thead><tr>';
-    
-    // Generate table headers
-
-    Object.keys(parsedData[0]).forEach(key => 
+    searchBtn.addEventListener('click', function () 
     {
-        tableHtml += `<th>${key}</th>`;
+        const searchInput = document.getElementById('searchInput').value.toLowerCase();
+        const tableRows = document.querySelectorAll('#tableContainer tbody tr');
+
+        tableRows.forEach(row => {
+            const rowData = row.textContent.toLowerCase();
+            if (rowData.includes(searchInput)) {
+                row.style.display = '';
+            }
+            else {
+                row.style.display = 'none';
+            }
+        });
     });
-    
-    tableHtml += '</tr></thead><tbody>';
-  
-    // Generate table rows
-    
-    parsedData.forEach(row => 
-    {
-        tableHtml += '<tr>';
-        Object.values(row).forEach(value => 
-        {
-            tableHtml += `<td>${value}</td>`;
+
+    // Displaying the content from the document.
+
+    function displayData(data) {
+        const parsedData = Papa.parse(data, { header: true }).data;
+        let tableHtml = '<table class="table"><thead><tr>';
+
+        // Generate table headers
+
+        Object.keys(parsedData[0]).forEach(key => {
+            tableHtml += `<th>${key}</th>`;
         });
 
-        tableHtml += '</tr>';
-    });
-  
-    tableHtml += '</tbody></table>';
-  
-    document.getElementById('tableContainer').innerHTML = tableHtml;
-}
+        tableHtml += '</tr></thead><tbody>';
+
+        // Generate table rows
+
+        parsedData.forEach(row => {
+            tableHtml += '<tr>';
+            Object.values(row).forEach(value => {
+                tableHtml += `<td>${value}</td>`;
+            });
+
+            tableHtml += '</tr>';
+        });
+
+        tableHtml += '</tbody></table>';
+
+        document.getElementById('tableContainer').innerHTML = tableHtml;
+    }
+
+});
