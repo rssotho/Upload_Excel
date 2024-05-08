a7488fc48fd46b40941c4069effa61ceb5dcb76f
rssotho <166599004+rssotho@users.noreply.github.com>
2024-05-08 09:43:30 +0200

    Using the data table link to load the content

--- script/script.js
+++ script/script.js
@@ -13,11 +13,31 @@ $(document).ready(function ()
         reader.onload = function (e) 
         {
             const data = e.target.result;
-            const workbook = XLSX.read(data, { type: 'array' });
-            const sheetName = workbook.SheetNames[0];
-            const csvData = XLSX.utils.sheet_to_csv(workbook.Sheets[sheetName]);
-            displayData(csvData);
+        const workbook = XLSX.read(data, { type: 'binary' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+        // Clear existing table data
+        const table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+        table.innerHTML = '';
+
+        jsonData.forEach(function(row) 
+        {
+          const tableRow = document.createElement('tr');
+          row.forEach(function(cell) {
+            const tableCell = document.createElement('td');
+            tableCell.textContent = cell;
+            tableRow.appendChild(tableCell);
+          });
+          table.appendChild(tableRow);
+        });
+        $('#dataTable').DataTable();
+
+
         };
+        reader.readAsBinaryString(file);
+        
 
         $('.btns').show();
         $('#search').show();
